607bee7a1847f86e457e52db4c67dfac76b22f48
Jhon Cruz <jhon.unal@gmail.com>
2019-08-11 11:07:11 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View><Text>Hola2</Text></View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
